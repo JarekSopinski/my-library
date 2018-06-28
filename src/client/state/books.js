3ba48a722d4a1e83bf8905b2db7c66f428d966b6
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-28 22:09:25 +0200

    Set up thunk middleware in Store, added books state

---
+++ src/client/state/books.js
@@ -0,0 +1,10 @@
+const initialState = {
+    books: []
+};
+
+export default (state = initialState, action = {}) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
--- src/client/store.js
+++ src/client/store.js
@@ -1,12 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import books from './state/books';
 
 const reducer = combineReducers({
-
+    books
 });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+    composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
