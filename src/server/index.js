2323c50be8b373eac6f20d1cd4e25dcdac91e769
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-28 15:31:30 +0200

    Set up MongoDB connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+const portNumber = 8080;
+const databaseUrl = 'mongodb://dbAdmin:db1357@ds249079.mlab.com:49079/my-library';
+
+mongoose.connect(databaseUrl);
+mongoose.Promise = global.Promise;
 
 app.use(express.static('dist'));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+app.listen(process.env.port || portNumber, () => console.log(`Listening on port ${portNumber}`));
