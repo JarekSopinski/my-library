79224364fa9dee3591653fcd0aea32e6b991db90
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-28 15:53:08 +0200

    Set up API routes

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const apiRoutes = require('./routes/api');
+
 const app = express();
 const portNumber = 8080;
 const databaseUrl = 'mongodb://dbAdmin:db1357@ds249079.mlab.com:49079/my-library';
@@ -10,5 +12,6 @@ mongoose.connect(databaseUrl);
 mongoose.Promise = global.Promise;
 
 app.use(express.static('dist'));
+app.use('/api', apiRoutes);
 
 app.listen(process.env.port || portNumber, () => console.log(`Listening on port ${portNumber}`));
---
+++ src/server/routes/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const path = '/books';
+
+router.get(path, (req, res) => {
+    res.send({type:'GET'})
+});
+
+router.post(path, (req, res) => {
+    res.send({type:'POST'})
+});
+
+router.put(`${path}/:id`, (req, res) => {
+    res.send({type:'PUT'})
+});
+
+router.delete(`${path}/:id`, (req, res) => {
+    res.send({type:'DELETE'})
+});
+
+module.exports = router;
