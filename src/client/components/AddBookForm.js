359e262d3258397ca1b271f013ea5b525ec99c59
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-29 15:54:39 +0200

    Add new book form, styled with SUI

--- src/client/components/AddBookForm.js
+++ src/client/components/AddBookForm.js
@@ -1,10 +1,104 @@
 import React, { Component } from 'react';
+import { Container, Form, Input } from 'semantic-ui-react';
+
+const initialState = {
+  title: '',
+  author: '',
+  isbn: '',
+  pages: '',
+  rating: ''
+};
 
 class AddBookForm extends Component {
 
-    render() {
+  state = initialState;
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+        [name]: value
+    })
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    this.setState(initialState)
+  };
+
+  render() {
+
+    const { title, author, isbn, pages, rating } = this.state;
+
         return (
-          <div></div>
+        <Container>
+        <Form onSubmit={this.handleSubmit}>
+
+          <Form.Group widths='equal'>
+
+            <Form.Field required>
+              <label>Title</label>
+              <Input 
+                fluid 
+                placeholder="Enter book's title"
+                name="title"
+                value={title}
+                onChange={this.handleChange}
+               />
+            </Form.Field>
+
+            <Form.Field required>
+              <label>Author</label>
+              <Input 
+                fluid 
+                placeholder="Enter book's author"
+                name="author"
+                value={author}
+                onChange={this.handleChange}
+               />
+            </Form.Field>
+
+            <Form.Field required>
+              <label>ISBN</label>
+              <Input 
+                fluid 
+                placeholder='Enter ISBN number' 
+                name="isbn"
+                value={isbn}
+                onChange={this.handleChange}
+                />
+            </Form.Field>
+
+            <Form.Field>
+              <label>Pages</label>
+              <Input 
+                fluid 
+                placeholder='Enter the number of pages' 
+                name="pages"
+                value={pages}
+                onChange={this.handleChange}
+                />
+            </Form.Field>
+
+            <Form.Field 
+              label='Rating' 
+              control='select'
+              name="rating"
+              value={rating}
+              onChange={this.handleChange}
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </Form.Field>
+          
+          </Form.Group>
+
+          <Form.Button>Add a new book</Form.Button>
+          
+        </Form>
+        </Container>
         );
       }
 
