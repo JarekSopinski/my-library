8634066ea4c7405aa5ff6a43d5f3175d79596f8e
JarekSopinski <jarek.sopinski@gmail.com>
2018-06-29 11:31:21 +0200

    Rendering data in BookListItem

--- src/client/components/BookListItem.js
+++ src/client/components/BookListItem.js
@@ -4,8 +4,18 @@ class BookListItem extends Component {
 
   render() {
 
+    const { title, author, isbn, pages, rating } = this.props.book;
+
     return (
-      <li></li>
+      <li>
+        <h2>{title}</h2>
+        <h3>{author}</h3>
+        <span>ISBN: {isbn}</span>
+        <br/>
+        { pages && <span>Pages: {pages}</span> }
+        <br/>
+        { rating && <span>Rating: {rating}</span> }
+      </li>
       );
 
   }
